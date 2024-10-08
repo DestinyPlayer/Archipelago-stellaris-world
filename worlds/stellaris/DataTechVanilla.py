bc799a69b6eff0d6605a3d05a998e881031e6bab
BootsinSoots <102177943+BootsinSoots@users.noreply.github.com>
2024-10-08 02:31:26 -0400

    Added non-weapon vanilla technologies
    created key-list pairs for organized vanilla technologies excluding weapons. Weapons will occur in the next commit

--- worlds/stellaris/DataTechVanilla.py
+++ worlds/stellaris/DataTechVanilla.py
@@ -34,5 +34,385 @@ vanillaTechs = {
         "tech_wormhole_stabilization",
         "tech_gateway_activation tech_gateway_construction"
     ]
+    "repeatable engineering": [ # non-progressive
+        "tech_repeatable_improved_tile_mineral_output",
+        "tech_repeatable_improved_military_station_damage tech_repeatable_improved_military_station_health",
+        "tech_repeatable_improved_armor_output",
+        "tech_repeatable_weapon_type_explosive_damage tech_repeatable_weapon_type_explosive_fire_rate",
+        "tech_repeatable_weapon_type_kinetic_damage tech_repeatable_weapon_type_kinetic_fire_rate",
+        "tech_repeatable_reduced_building_time"
+        "
+    ]
+    "corvette upgrades": [
+        "tech_corvette_build_speed",
+        "tech_corvette_hull_1",
+        "tech_corvette_hull_2"
+    ]
+    "destroyer upgrades": [
+        "tech_destroyer_build_speed",
+        "tech_destroyer_hull_1",
+        "tech_destroyer_hull_2"
+    ]
+    "cruiser upgrades": [
+        "tech_cruiser_build_speed",
+        "tech_cruiser_hull_1",
+        "tech_cruiser_hull_2"
+    ]
+    "battleship upgrades": [
+        "tech_battleship_build_speed",
+        "tech_battleship_hull_1",
+        "tech_battleship_hull_2"
+    ]
+    "titan upgrades": [
+        "tech_titan_hull_1",
+        "tech_titan_hull_2"
+    ]
+    "starbase upgrades": [
+        "tech_modular_engineering",
+        "tech_space_defense_station_improvement"
+    ]
+    "strike craft": [
+        "tech_strike_craft_1",
+        "tech_strike_craft_2",
+        "tech_strike_craft_3",
+        "tech_strike_craft_skrand"
+    ]
+    "orbital habitats": [
+        "tech_habitat_1",
+        "tech_habitat_2",
+        "tech_habitat_3"
+    ]
+    "ship armor": [
+        "tech_ship_armor_2",
+        "tech_ship_armor_3",
+        "tech_ship_armor_4",
+        "tech_ship_armor_5"
+    ]
+    "armor hardener": [
+        "tech_armor_hardeners_1",
+        "tech_armor_hardeners_2"
+    ]
+    "robotics": [
+        "tech_powered_exoskeletons",
+        "tech_robotic_workers tech_robomodding",
+        "tech_droid_workers",
+        "tech_synthetic_workers tech_robot_assembly_complex",
+        "tech_synthetic_leaders"
+    ]
+    "space mining": [
+        "tech_space_mining_1",
+        "tech_space_mining_2",
+        "tech_space_mining_3",
+        "tech_space_mining_4",
+        "tech_space_mining_5"
+    ]
+    "planetary mining": [
+        "tech_mining_1 tech_mineral_purification_1",
+        "tech_mining_2 tech_mineral_purification_2",
+        "tech_mining_3"
+    ]
+    "alloy production": [
+        "tech_alloys_1 tech_advanced_metallurgy_1",
+        "tech_alloys_2 tech_advanced_metallurgy_2"
+    ]
+    "consumer goods": [
+        "tech_luxuries_1 tech_consumer_good_refinement_1",
+        "tech_luxuries_2 tech_consumer_good_refinement_2"
+    ]
+    "afterburners": [
+        "tech_afterburners_1",
+        "tech_afterburners_2"
+    ]
+    "planetary build speed": [
+        "tech_assembly_pattern",
+        "tech_construction_templates"
+    ]
+    "housing": [
+        "tech_housing_1",
+        "tech_housing_2",
+        "tech_leviathan_techgenesis"
+    ]
+    "strategic resource type": [ # non-progressive
+        "tech_mine_volatile_motes tech_volatile_motes",
+        "tech_mine_exotic_gases tech_exotic_gases",
+        "tech_mine_rare_crystals tech_rare_crystals",
+        
+    ]
+    "strategic resource split": [ # non-progressive
+        "tech_mine_volatile_motes tech_mine_exotic_gases tech_mine_rare_crystals"
+        "tech_volatile_motes tech_exotic_gases tech_rare_crystals"
+    ]
+    "shields": [
+        "tech_shields_2",
+        "tech_shields_3",
+        "tech_shields_4",
+        "tech_shields_5",
+        "tech_dark_matter_deflector"
+    ]
+    "science lab": [
+        "tech_basic_science_lab_2",
+        "tech_basic_science_lab_3",
+        "tech_global_research_initiative"
+    ]
+    "station science": [
+        "tech_space_science_1",
+        "tech_space_science_2",
+        "tech_space_science_3",
+        "tech_space_science_4",
+        "tech_space_science_5"
+    ]
+    "colony ship": [
+        "tech_cryostasis_1",
+        "tech_cryostasis_2"
+    ]
+    "research ai": [
+        "tech_administrative_ai",
+        "tech_self_aware_logic",
+        "tech_sapient_ai tech_positronic_implants"
+    ]
+    "ship combat ai": [
+        "tech_combat_computers_1",
+        "tech_combat_computers_2",
+        "tech_combat_computers_3 tech_combat_computers_autonomous"
+    ]
+    "encryption": [
+        "tech_encryption_1",
+        "tech_encryption_2",
+        "tech_encryption_3"
+    ]
+    "decryption": [
+        "tech_decryption_1",
+        "tech_decryption_2",
+        "tech_decryption_3"
+    ]
+    "reactor booster": [
+        "tech_reactor_boosters_1",
+        "tech_reactor_boosters_2",
+        "tech_reactor_boosters_3"
+    ]
+    "shield hardener": [
+        "tech_shield_hardeners_1",
+        "tech_shield_hardeners_2"
+    ]
+    "sensors": [
+        "tech_sensors_2",
+        "tech_sensors_3",
+        "tech_sensors_4"
+    ]
+    "power plants": [
+        "tech_power_plant_2 tech_power_hub_1",
+        "tech_power_plant_3 tech_power_hub_2",
+        "tech_power_plant_4"
+    ]
+    "repeatable physics": [
+        "tech_repeatable_improved_tile_energy_output",
+        "tech_repeatable_improved_shield_output",
+        "tech_repeatable_weapon_type_energy_damage tech_repeatable_weapon_type_energy_fire_rate",
+        "tech_repeatable_reduced_building_cost"
+    ]
+    "trade": [
+        "tech_space_trading",
+        "tech_interstellar_economics",
+        "tech_galactic_markets"
+    ]
+    "farming": [
+        "tech_eco_simulation tech_hydroponics",
+        "tech_gene_crops tech_food_processing_1",
+        "tech_nano_vitality_crops tech_food_processing_2",
+        "tech_nutrient_replication"
+    ]
+    "habitability": [
+        "tech_colonization_2",
+        "tech_colonization_3",
+        "tech_colonization_4",
+        "tech_colonization_5"
+    ]
+    "hospital": [
+        "tech_frontier_health",
+        "tech_frontier_hospital"
+    ]
+    "terrain blockers": [ # non-progressive
+        "tech_mountain_range",
+        "tech_volcano",
+        "tech_dangerous_wildlife",
+        "tech_dense_jungle",
+        "tech_quicksand_basin",
+        "tech_noxious_swamp",
+        "tech_massive_glacier",
+        "tech_toxic_kelp",
+        "tech_deep_sinkhole"
+    ]
+    "terraforming": [
+        "tech_terrestrial_sculpting",
+        "tech_ecological_adaptation",
+        "tech_climate_restoration"
+    ]
+    "leader genetics": [
+        "tech_vitality_boosters",
+        "tech_selected_lineages",
+        "tech_capacity_boosters"
+    ]
+    "gene tailoring": [
+        "tech_genome_mapping",
+        "tech_cloning",
+        "tech_epigenetic_triggers",
+        "tech_gene_tailoring",
+        "tech_glandular_acclimation",
+        "tech_gene_expressions"
+    ]
+    "army genetics": [
+        "tech_gene_banks",
+        "tech_morphogenetic_field_mastery",
+        "tech_gene_seed_purification"
+    ]
+    "transgenesis": [ #non-progressive
+        "tech_lithoid_transgenesis",
+        "tech_plantoid_transgenesis",
+        "tech_leviathan_transgenesis"
+    ]
+    "fleet size": [
+        "tech_doctrine_fleet_size_1",
+        "tech_doctrine_fleet_size_2",
+        "tech_doctrine_fleet_size_3",
+        "tech_doctrine_fleet_size_4",
+        "tech_doctrine_fleet_size_5"
+    ]
+    "naval capacity": [
+        "tech_doctrine_navy_size_1",
+        "tech_doctrine_navy_size_2",
+        "tech_doctrine_navy_size_3",
+        "tech_doctrine_navy_size_4"
+    ]
+    "army defense": [
+        "tech_ground_defense_planning",
+        "tech_global_defense_grid"
+    ]
+    "army offense": [
+        "tech_centralized_command",
+        "tech_combat_training"
+    ]
+    "psionics": [
+        "tech_psionic_theory",
+        "tech_telepathy",
+        "tech_precognition_interface tech_thought_enforcement"
+    ]
+    "starbase capacity": [
+        "tech_galactic_ambitions",
+        "tech_manifest_destiny"
+    ]
+    "campaigns": [
+        "tech_interstellar_campaigns",
+        "tech_galactic_campaigns"
+    ]
+    "planet government": [
+        "tech_planetary_unification",
+        "tech_colonial_centralization",
+        "tech_galactic_administration"
+    ]
+    "planet infrastructure": [
+        "tech_planetary_infrastructure_1",
+        "tech_planetary_infrastructure_2"
+    ]
+    "bureaucracy": [
+        "tech_adaptive_bureaucracy",
+        "tech_effective_bureaucracy",
+        "tech_colonial_bureaucracy",
+        "tech_galactic_bureaucracy"
+    ]
+    "thralls": [
+        "tech_neural_implants",
+        "tech_slave_colonies"
+    ]
+    "unity sites": [
+        "tech_heritage_site tech_holographic_rituals tech_hive_cluster",
+        "tech_hypercomms_forum tech_consecration_fields tech_hive_confluence",
+        "tech_autocurating_vault tech_transcendent_faith"
+    ]
+    "psionic shield": [
+        "tech_psionic_barrier",
+        "tech_psionic_shield"
+    ]
+    "embassy": [
+        "tech_xeno_diplomacy",
+        "tech_xeno_relations"
+    ]
+    "capital production": [
+        "tech_capital_productivity_1",
+        "tech_capital_productivity_2",
+        "tech_capital_productivity_3"
+    ]
+    "psionic covenants": [ # non-progressive
+        "tech_covenant_composer",
+        "tech_covenant_eater",
+        "tech_covenant_instrument",
+        "tech_covenant_whisperers"
+    ]
+    "nodes": [
+        "tech_node_reformatting_1 tech_node_culling_1",
+        "tech_node_reformatting_2 tech_node_culling_2",
+        "tech_node_reformatting_3 tech_node_culling_3"
+    ]
+    "bioreactor": [
+        "tech_bio_reactor",
+        "tech_bio_reactor_2"
+    ]
+    "repeatable society": [
+        "tech_repeatable_improved_tile_food_output",
+        "tech_repeatable_improved_core_system_cap",
+        "tech_repeatable_improved_army_damage tech_repeatable_improved_army_health",
+        "tech_repeatable_improved_leader_life_span",
+        "tech_repeatable_naval_cap tech_repeatable_command_limit",
+        "tech_repeatable_improved_starbase_capacity",
+        "tech_repeatable_weapon_type_strike_craft_fire_damage tech_repeatable_weapon_type_strike_craft_fire_rate"
+    ]
+    "
+    "miscellaneous": [  # non-progressive
+        "tech_integrated_cybernetics",
+        "tech_mine_betharian",
+        "tech_mine_living_metal",
+        "tech_mine_zro",
+        "tech_mine_dark_matter",
+        "tech_nanite_transmutation",
+        "tech_defense_platform_hull_1",
+        "tech_automated_exploration",
+        "tech_auxiliary_fire_control",
+        "tech_synchronized_defences",
+        "tech_neuro_quantum_links",
+        "tech_shield_rechargers_1",
+        "tech_planetary_shield_generator",
+        "tech_ftl_inhibitor",
+        "tech_mega_engineering",
+        "tech_alien_life_studies",
+        "tech_tomb_world_adaption",
+        "tech_regenerative_hull_tissue",
+        "tech_interstellar_fleet_traditions",
+        "tech_refit_standards",
+        "tech_command_matrix",
+        "tech_subdermal_stimulation",
+        "tech_tracking_implants",
+        "tech_global_production_strategy",
+        "tech_living_state tech_autonomous_agents",
+        "tech_collective_self tech_embodied_dynamism",
+        "tech_artificial_moral_codes tech_unity_of_purpose",
+        "tech_synthetic_thought_patterns tech_collective_production_methods",
+        "tech_resource_processing_algorithms" # gestalt only
+        "tech_hyper_entertainment_forum",
+        "tech_paradise_dome",
+        "tech_ascension_theory",
+        "tech_penal_colonies",
+        "tech_resort_colonies",
+        "tech_housing_agrarian_idyll",
+        "tech_xeno_linguistics" # requires Galactic Paragons
+        "
+    ]
+    "event": [ # non-progressive
+        "tech_crystal_armor_1",
+        "tech_crystal_armor_2",
+        "tech_curator_lab",
+        "tech_archeology_lab",
+        "tech_prescient_data_modeling", 
+        "
+    ]
+        
 
 }
