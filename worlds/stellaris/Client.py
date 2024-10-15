9066b07a471521b3e5d0fcfde40551c1a1c2237a
DestinyPlayer <thenextdecade244@gmail.com>
2024-10-15 21:57:37 +0300

    Make the base loop EVEN FASTER

--- worlds/stellaris/Client.py
+++ worlds/stellaris/Client.py
@@ -184,7 +184,7 @@ async def loopTransmit():
         grabResources()
         receiveItem()
         sendItem()
-        await asyncio.sleep(0.01)
+        await asyncio.sleep(0.001)
 
 
 def runStellarisClient(*args):
@@ -254,7 +254,7 @@ def runStellarisClient(*args):
                 await finalSendType("physics")
                 await finalSendType("society")
                 await finalSendType("engineering")
-            await asyncio.sleep(0.01)
+            await asyncio.sleep(0.001)
 
     async def main(args):
         ctx = StellarisContext(args.connect, args.password)
